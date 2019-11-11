e26a03d1b2f1f4cdf238189a6a33dff2e992d9ec
Ben <thisbenrogers@gmail.com>
2019-11-11 11:34:53 -0600

    :sparkles: Adds /api/hash route

--- api/api-router.js
+++ api/api-router.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
+const bcrypt = require('bcryptjs');
+
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
@@ -9,5 +11,15 @@ router.use('/users', usersRouter);
 router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
 });
+
+router.post('/hash', (req, res) => {
+  // read a pw from body
+  const { password } = req.body;
+  // hash pw using bcrypt.js
+  const hashed = bcrypt.hashSync(password, 14)
+  // return hashed pw to user in an object that looks like this:
+  // { password: 'og password', hash: 'hashed password' }
+  res.status(200).json({ password: password, hash: hashed })
+})
 
 module.exports = router;
